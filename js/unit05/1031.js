d05ab23fdb94f9974712fb979d0596480c765f19
李晏瑋 <magic556679@gmail.com>
2023-12-17 20:44:31 +0800

    feat 1031

---
+++ js/unit05/1031.js
@@ -0,0 +1,29 @@
+// 完全平方和
+var readline = require('readline')
+
+var lines = []
+var rl = readline.createInterface({
+  input: process.stdin,
+})
+
+rl.on('line', function (line) {
+  lines.push(line)
+})
+
+rl.on('close', function () {
+  solve(lines)
+})
+
+function solve(lines) {
+  const tmp = Number(lines[0])
+  let count = 0
+  for (let i = 1; i < tmp + 1; i++) {
+    let square = i ** 2
+    if (tmp > square) {
+      count += i ** 2
+    }
+  }
+  console.log(count)
+}
+
+solve(['30'])
