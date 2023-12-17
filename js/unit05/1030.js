2982b8b7195a3d68c5d79b94fd126cc32451c2ee
李晏瑋 <magic556679@gmail.com>
2023-12-17 20:12:20 +0800

    feat 1030

---
+++ js/unit05/1030.js
@@ -0,0 +1,30 @@
+// 加減乘除
+// var readline = require('readline')
+
+// var lines = []
+// var rl = readline.createInterface({
+//   input: process.stdin,
+// })
+
+// rl.on('line', function (line) {
+//   lines.push(line)
+// })
+
+// rl.on('close', function () {
+//   solve(lines)
+// })
+
+function solve(lines) {
+  const tmp = lines[0].split('')
+  let newStr = ''
+  for (let i = tmp.length - 1; i >= 0; i--) {
+    newStr += tmp[i]
+  }
+  if (lines[0] === newStr) {
+    console.log('True')
+  } else {
+    console.log('False')
+  }
+}
+
+// solve(['abbbbc'])
